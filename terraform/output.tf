815f825f13dfb80f014320c0f7702c42a8fd17ec
ivanovge <georgi.ivanov@helecloud.com>
2020-07-02 20:17:33 +0300

    output wordpress admin user and password post apply

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
-output "lb_url" {
+output "Wordpress_URL" {
   value = aws_alb.alb.dns_name
+}
+
+output "wp_admin_user" {
+  value = var.wp_admin_user
+}
+
+output "wp_admin_password" {
+  value = random_password.wp_admin_pass.result
 }
