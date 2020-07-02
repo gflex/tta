42ee27da9a8a823ec5d64ecd7e3c69c0d156854f
ivanovge <georgi.ivanov@helecloud.com>
2020-07-02 20:12:53 +0300

    typos in volume names

--- terraform/07_db.tf
+++ terraform/07_db.tf
@@ -78,7 +78,7 @@ resource "aws_instance" "db_host" {
   availability_zone      = local.avz_map.avz1
   key_name               = aws_key_pair.deployer.key_name
   user_data_base64       = base64encode(data.template_file.tpl_db_user_data.rendered)
-  volume_tags            = merge({ "Name" : "${var.project}_ebs_db_host" }, local.common_tags)
+  volume_tags            = merge({ "Name" : "${var.project}_ebs_db_vol" }, local.common_tags)
   tags                   = merge({ "Name" : "${var.project}_db_host" }, local.common_tags)
 }
 
@@ -86,7 +86,7 @@ resource "aws_instance" "db_host" {
 resource "aws_ebs_volume" "ebs_db_data" {
   availability_zone = local.avz_map.avz1
   size              = "10"
-  tags              = merge({ "Name" : "${var.project}_ebs_db_host" }, local.common_tags)
+  tags              = merge({ "Name" : "${var.project}_ebs_db_vol" }, local.common_tags)
 }
 
 ##Attach the volume
