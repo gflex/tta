6d7573c84ed694846965e3522f044d09d1608411
ivanovge <georgi.ivanov@helecloud.com>
2020-07-02 21:24:17 +0300

    sanitize post bootstrap by removing playbook and uninstall ansible

--- terraform/files/db_user_data.sh
+++ terraform/files/db_user_data.sh
@@ -5,3 +5,5 @@ ssm_param="${ANS_APP_SSM}"
 amazon-linux-extras install ansible2
 aws ssm get-parameter --name "$ssm_param" --region=$region --query Parameter.Value --output text |base64 -d > $playbook
 /usr/bin/ansible-playbook $playbook
+rm $playbook
+yum remove -y ansible
