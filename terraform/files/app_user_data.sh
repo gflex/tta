f613453f4dda835a12474d89031e6db62fe5777b
Georgi Ivanov <geflex@gmail.com>
2020-07-05 19:18:39 +0300

    ansible playbooks as heredocs in user data script

--- terraform/files/app_user_data.sh
+++ terraform/files/app_user_data.sh
@@ -1,9 +1,77 @@
 #!/usr/bin/env bash
 playbook="/tmp/wp_ansible.yml"
 region="${REGION}"
-ssm_param="${ANS_APP_SSM}"
 amazon-linux-extras install ansible2
-aws ssm get-parameter --name "$ssm_param" --region="$region" --query Parameter.Value --output text |base64 -d > "$playbook"
+#aws ssm get-parameter --name "$ssm_param" --region="$region" --query Parameter.Value --output text |base64 -d > "$playbook"
+cat > $playbook << EOPB
+---
+- name: configure web server
+  hosts: localhost
+  connection: local
+  gather_facts: no
+  vars:
+    wp_dir: "${WPDIR}"
+    admin_mail: "${ADMIN_MAIL}"
+    project: "${PROJECT}"
+
+  tasks:
+    - name: install needed software
+      yum:
+        name:
+          - httpd
+          - python2-pip
+          - mysql
+        state: installed
+    - name: install pip
+      pip:
+        name:
+          - boto3
+          - botocore
+    - name: get ssm data
+      set_fact:
+        wp_db: "{{ lookup('aws_ssm', '${WP_DB_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+        wp_db_name: "{{ lookup('aws_ssm', '${WP_DB_NAME_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+        wp_admin: "{{ lookup('aws_ssm', '${WP_ADMIN_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+        wp_blog: "{{ lookup('aws_ssm', '${WP_BLOG_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+      no_log: True
+    - name: install wordpress
+      shell: "{{ item }}"
+      with_items:
+        - "amazon-linux-extras install -y epel php7.3"
+        - "yum install -y wp-cli"
+        - "/usr/bin/wp --path={{ wp_dir }} core download"
+        - "/usr/bin/wp --path={{ wp_dir }} core config --dbname={{ wp_db_name.name }} --dbuser={{ wp_db.user }} --dbpass=\'{{ wp_db.password }}\' --dbhost={{ wp_db_name.host }} --dbprefix=wp_"
+        - "/usr/bin/wp --path={{ wp_dir }} core install --url={{ wp_db.url }} --title=wordpress --admin_user={{ wp_admin.user }} --admin_password=\'{{ wp_admin.password }}\' --admin_email={{ admin_mail }}"
+      no_log: True
+    - name: fix dir permissions
+      file:
+        path: "{{ wp_dir }}"
+        recurse: yes
+        owner: ec2-user
+        group: apache
+        mode: u=rwX,g=rwX,o=rX
+    - name: start httpd
+      service:
+        name: httpd
+        state: restarted
+        enabled: yes
+    - name: get instance facts
+      uri:
+        url: "http://169.254.169.254/latest/dynamic/instance-identity/document"
+        return_content: true
+        headers:
+          Content-type: "application/json"
+      register: result
+    - name: set hostname
+      hostname:
+        name: "{{ result.json.instanceId }}"
+    - name: set tag and update post
+      shell: |
+        aws ec2 create-tags --region "{{ result.json.region }}" --resources "{{ result.json.instanceId }}" --tags Key=Name,Value="{{ project }}-wp-{{ result.json.pendingTime }}"
+        wp post update 1 --path="{{ wp_dir }}" --post_content="{{ wp_blog.post1 }}" --post_title="Linux namespaces" --post_name="Linux namespaces" --post_status=publish --post_author="{{ wp_admin.user }}"
+        wp comment delete 1 --path="{{ wp_dir }}"
+
+EOPB
 /usr/bin/ansible-playbook $playbook
 rm $playbook
 yum remove -y ansible
--- terraform/files/db_user_data.sh
+++ terraform/files/db_user_data.sh
@@ -1,9 +1,97 @@
 #!/usr/bin/env bash
 playbook="/tmp/db_ansible.yml"
 region="${REGION}"
-ssm_param="${ANS_APP_SSM}"
 amazon-linux-extras install ansible2
-aws ssm get-parameter --name "$ssm_param" --region=$region --query Parameter.Value --output text |base64 -d > $playbook
+#aws ssm get-parameter --name "$ssm_param" --region=$region --query Parameter.Value --output text |base64 -d > $playbook
+cat > $playbook << EOPB
+---
+- name: configure db host
+  hosts: localhost
+  connection: local
+  gather_facts: no
+  vars:
+    - db_dir: "${DB_ROOT_DIR}"
+    - mysql_user: "mysql"
+    - mysql_group: "mysql"
+    - project: "${PROJECT}"
+  tasks:
+    - name: create file system
+      filesystem:
+        dev: /dev/xvdh
+        fstype: ext4
+    - name: mount FS
+      mount:
+        src: /dev/xvdh
+        path: '{{ db_dir }}'
+        state: mounted
+        fstype: ext4
+    - name: install mariadb
+      yum:
+        name:
+          - mariadb-server
+          - python2-pip
+        state: installed
+    - name: chown_db_dir
+      file:
+        path: '{{ db_dir }}'
+        owner: '{{ mysql_user }}'
+        group: '{{ mysql_group }}'
+        mode: '1755'
+    - name: start mariadb
+      service:
+        name: mariadb
+        state: started
+        enabled: yes
+    - name: install pip
+      pip:
+        name:
+          - boto3
+          - botocore
+          - PyMySQL
+    - name: ssm data
+      set_fact:
+        db_root: "{{ lookup('aws_ssm', '${DB_ROOT_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+        wp_db: "{{ lookup('aws_ssm', '${WP_DB_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+        wp_db_name: "{{ lookup('aws_ssm', '${WP_DB_NAME_SSM}', region='${REGION}', shortnames=true, bypath=true, recursive=true) }}"
+      no_log: True
+    - name: get facts
+      uri:
+        url: "http://169.254.169.254/latest/dynamic/instance-identity/document"
+        return_content: true
+        headers:
+          Content-type: "application/json"
+      register: result
+    - name: set hostname
+      hostname:
+        name: "{{ result.json.instanceId }}"
+    - name: change root pw
+      mysql_user:
+        name: "{{ db_root.user }}"
+        password: "{{ db_root.password }}"
+        login_user: "{{ db_root.user }}"
+        check_implicit_admin: yes
+        state: present
+      no_log: true
+    - name: create DB
+      mysql_db:
+        login_user: "{{ db_root.user }}"
+        login_password: "{{ db_root.password }}"
+        name: "{{ wp_db_name.name }}"
+        state: present
+    - name: create user
+      mysql_user:
+        login_user: "{{ db_root.user }}"
+        login_password: "{{ db_root.password }}"
+        name: "{{ wp_db.user }}"
+        password: "{{ wp_db.password }}"
+        priv: "{{ wp_db_name.name }}.*:ALL,GRANT"
+        host: "{{ item }}"
+        state: present
+      no_log: true
+      with_items:
+        - "%"
+        - "localhost"
+EOPB
 /usr/bin/ansible-playbook $playbook
 rm $playbook
 yum remove -y ansible
