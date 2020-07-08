f58472768958cc4c597553a6ad270183340d7c22
ivanovge <georgi.ivanov@helecloud.com>
2020-07-08 13:40:36 +0300

    fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ variable "project" {
 locals {
   common_tags = {
     applicant = var.applicant
-    project = var.project
+    project   = var.project
   }
 }
 
@@ -30,40 +30,40 @@ variable "private_nets" {
 
 variable "db_inst_type" {
   description = "Instance type for DB host"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "app_inst_type" {
   description = "ec2 insance type for the web servers"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "db_root_user" {
   description = "username for the db"
-  default = "root"
+  default     = "root"
 }
 
 variable "db_wp_user" {
   description = "db user for wordpress"
-  default = "wp_db_user"
+  default     = "wp_db_user"
 }
 
 variable "wp_db_name" {
   description = "database for wordpress"
-  default = "wp_db"
+  default     = "wp_db"
 }
 
 variable "wp_admin_user" {
   description = "Username of wordpress administrator"
-  default = "wpadmin"
+  default     = "wpadmin"
 }
 
 variable "wp_root_dir" {
   description = "root dir for wordpress installation"
-  default = "/var/www/html"
+  default     = "/var/www/html"
 }
 
 variable "db_root_dir" {
   description = "database root directory"
-  default = "/var/lib/mysql"
+  default     = "/var/lib/mysql"
 }
