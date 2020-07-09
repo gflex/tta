7b33085fa68f5cb697e47fdb1c42ac5de361df31
ivanovge <georgi.ivanov@helecloud.com>
2020-07-09 15:05:48 +0300

    remove modules

--- terraform/modules/subnet/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "subnet" {
-  for_each                = var.m_net_def
-  vpc_id                  = var.m_vpc_id
-  cidr_block              = each.value["cidr"]
-  availability_zone       = lookup(var.m_subnets_avz, each.value["avz"])
-  map_public_ip_on_launch = var.m_public
-  tags                    = merge({ "Name" = each.key }, var.m_common_tags)
-}
--- terraform/modules/subnet/output.tf
+++
@@ -1,3 +0,0 @@
-output "subnets_ids" {
-  value = values(aws_subnet.subnet)[*].id
-}
