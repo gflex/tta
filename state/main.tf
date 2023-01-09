ac63faa8f1f8bc8ac50bafec939602abb1204ea8
Georgi Ivanov <g.ivanov@sap.com>
2023-01-09 14:44:16 +0200

    adapt S3 to tf provider version 4

--- state/main.tf
+++ state/main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = var.aws_region
+  default_tags {
+    tags = local.common_tags
+  }
 }
 
 terraform {
@@ -14,17 +17,30 @@ terraform {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.project}-${var.applicant}-tf-remote-state-bucket"
-  versioning {
-    enabled = true
-  }
-  acl = "private"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  tags = local.common_tags
 }
 
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+output "terraform_state" {
+  value = aws_s3_bucket.terraform_state.id
+}
